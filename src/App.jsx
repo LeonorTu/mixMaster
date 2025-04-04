c0bee2bacccefe847de25023b88a2b935fc670ca
Leonor Tu <Jingyi.Tu@outlook.com>
2025-04-04 10:59:31 +0300

    feat: implement routing with React Router, add main pages and layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,40 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  About,
+  HomeLayout,
+  Landing,
+  Error,
+  Newsletter,
+  Cocktail,
+} from './pages'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    children: [
+      {
+        index:true,
+        element: <Landing />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'newsletter',
+        element: <Newsletter />,
+      },
+      {
+        path: 'cocktail',
+        element: <Cocktail />,
+      },
+    ],
+  },
+])
+
 const App = () => {
-  return <h2>mixmaster starter</h2>;
-};
-export default App;
+  return <RouterProvider router={router} />
+}
+
+export default App
