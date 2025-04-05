7e8641c7fc7f37df589ac49c71db191e2f184a53
Leonor Tu <Jingyi.Tu@outlook.com>
2025-04-05 20:23:20 +0300

    feat: enhance HomeLayout with loading state and update SinglePageError message

--- src/pages/SinglePageError.jsx
+++ src/pages/SinglePageError.jsx
@@ -4,6 +4,6 @@ const SinglePageError = () => {
   const error = useRouteError();
   console.log(error);
 
-  return <h2>{error.message}</h2>;
+  return <h2>Oops, there was an error...</h2>;
 };
 export default SinglePageError;
