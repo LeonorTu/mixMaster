4800538ace2b512c9c1266bef02517b8f789ba87
Leonor Tu <Jingyi.Tu@outlook.com>
2025-04-07 21:19:22 +0300

    feat: integrate React Query for cocktail search and add SearchForm component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,22 +1,44 @@
 import { useLoaderData } from 'react-router-dom';
 import axios from 'axios';
 import CocktailList from '../components/CocktailList';
+import SearchForm from '../components/SearchForm';
+import { QueryClient, useQuery } from '@tanstack/react-query';
+
 const cocktailSearchUrl =
-  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?';
 
-export const loader = async () => {
-  const searchTerm = 'a';
-  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-
-  return { drinks: response.data.drinks, searchTerm };
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ['search', searchTerm || 'all'],
+    queryFn: async () => {
+      const searchParam = searchTerm ? 's=' + searchTerm : 'f=a';
+      const response = await axios.get(`${cocktailSearchUrl}${searchParam}`);
+      return response.data.drinks;
+    },
+  };
 };
 
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const url = new URL(request.url);
+    const searchTerm = url.searchParams.get('search');
+    // const searchParam = searchTerm ? 's=' + searchTerm : 'f=a';
+    // const response = await axios.get(`${cocktailSearchUrl}${searchParam}`);
+    await queryClient.ensureQueryData(searchCocktailsQuery(searchTerm));
+    return { searchTerm };
+  };
+
 const Landing = () => {
-  const { drinks, searchTerm } = useLoaderData();
-  console.log(drinks);
+  const { searchTerm } = useLoaderData();
+  const { data: drinks, isLoading } = useQuery(
+    searchCocktailsQuery(searchTerm)
+  );
+  if (isLoading) return <h4>Loading...</h4>;
   return (
     <>
-      <CocktailList drinks={drinks}/>
+      <SearchForm searchTerm={searchTerm} />
+      <CocktailList drinks={drinks} />
     </>
   );
 };
